cbf446de13c2ff0b89205f9d8c3dee594128fed4
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-23 07:46:58 +0545

    Improvement: Axios Interceptor added

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,19 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import UseAxiosPrivate from "../hooks/UseAxiosPrivate";
 
-export const Users = () => {
+const Users = () => {
     const [users, setUsers] = useState<any>();
+    const axiosPrivate = UseAxiosPrivate();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getUsers = async () => {
+            try {
+                const response = await axiosPrivate.get('/users', {
+                    signal: controller.signal
+                });
+                console.log(response.data);
+                isMounted && setUsers(response.data);
+            } catch (err) {
+                console.error(err);
+                navigate('/login', { state: { from: location }, replace: true });
+            }
+        }
+
+        getUsers();
+
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
+    })
 
     return (
         <article>
             <h2>Users List</h2>
             {users?.length
-            ?(
-                <ul>
-                    {users?.map((user:any, key:number)=>
-                    <li key={key}>{user?.name}</li>)}
-                </ul>
-            ): <p>Users to display</p>
+                ? (
+                    <ul>
+                        {users.map((user: any, i: number) => <li key={i}>{user?.username}</li>)}
+                    </ul>
+                ) : <p>No users to display</p>
             }
         </article>
-    )
-}
+    );
+};
+
+export default Users;
